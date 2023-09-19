712784cf2cc8d521daa145a5b4e545af4db6c3da
Mark Anderson <manderson@diesellaptops.com>
2023-09-19 13:11:38 -0400

    updated login page and adjusted home page formatting, also added routing once logged in

---
+++ login.js
@@ -0,0 +1,42 @@
+const submitButton = document
+  .getElementById("submitButton")
+  .addEventListener("click", submit);
+
+const cancelButton = document
+  .getElementById("cancelButton")
+  .addEventListener("click", cancel);
+
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+let user = "mark777";
+let pass = "test";
+
+function submit(e) {
+  e.preventDefault();
+  if (validateUser(user) && validatePass(pass)) {
+    console.log("access granted");
+    window.location.replace("/Home.html");
+  } else {
+    console.log("access denied");
+  }
+}
+
+function cancel(e) {
+  e.preventDefault();
+  username.value = "";
+  password.value = "";
+}
+
+function validateUser(user) {
+  if (user === username.value) {
+    return true;
+  }
+  return false;
+}
+
+function validatePass(pass) {
+  if (pass === password.value) {
+    return true;
+  }
+  return false;
+}
